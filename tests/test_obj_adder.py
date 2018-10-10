5a2ed934e55b1caa84a1380d8c0a465143a5f247
dnnspark <dnns.i.park@gmail.com>
2018-10-10 12:42:38 -0700

    Add test_obj_adder.py.

---
+++ tests/test_obj_adder.py
@@ -0,0 +1,122 @@
+import os
+import numpy as np
+import pybullet as pb
+from bulletwrapper import BulletSimulator, StopSimulation
+from bulletwrapper.hooks import GroundPlaneHook, OBJCreatorHook
+import pybullet_data
+
+CONNECT_MODE = pb.DIRECT
+# CONNECT_MODE = pb.GUI
+
+DUCK_OBJ_PATH = os.path.join(pybullet_data.getDataPath(), 'duck.obj')
+
+
+def test_obj_adder_single():
+
+    ground_plane = GroundPlaneHook()
+
+    duck = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([0,0,1]),
+        )
+
+    sim = BulletSimulator(
+        mode=CONNECT_MODE,
+        max_time=20.,
+        hooks=[
+            ground_plane,
+            duck,
+            ],
+        )
+
+    sim.reset()
+    while True:
+        try:
+            sim.step()
+        except StopSimulation:
+            break;
+
+def test_obj_adder_multiple_in_sequence():
+
+    ground_plane = GroundPlaneHook()
+
+    height = 3.
+
+    duck_1 = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([0,0,height]),
+        time_to_create = 0.,
+        )
+
+    duck_2 = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([0,0,height]),
+        time_to_create = 1.,
+        )
+
+    duck_3 = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([0,0,height]),
+        time_to_create = 2.,
+        )
+
+    sim = BulletSimulator(
+        mode=CONNECT_MODE,
+        max_time=20.,
+        hooks=[
+            ground_plane,
+            duck_1,
+            duck_2,
+            duck_3,
+            ],
+        )
+
+    sim.reset()
+    while True:
+        try:
+            sim.step()
+        except StopSimulation:
+            break;
+
+def test_obj_adder_multiple_simultaneous():
+
+    ground_plane = GroundPlaneHook()
+
+    time_to_create = 1.
+
+
+    duck_1 = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([-1,-1,2]),
+        time_to_create = time_to_create,
+        )
+
+    duck_2 = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([0,0,4]),
+        time_to_create = time_to_create,
+        )
+
+    duck_3 = OBJCreatorHook(
+        path_to_obj = DUCK_OBJ_PATH,
+        position = np.array([1,1,6]),
+        time_to_create = time_to_create,
+        )
+
+    sim = BulletSimulator(
+        mode=CONNECT_MODE,
+        max_time=20.,
+        hooks=[
+            ground_plane,
+            duck_1,
+            duck_2,
+            duck_3,
+            ],
+        )
+
+    sim.reset()
+    while True:
+        try:
+            sim.step()
+        except StopSimulation:
+            break;
